00e17e2e90a26b33f87a96b146aafd1bc47ae36d
avielbst <93471148+avielbst@users.noreply.github.com>
2023-06-17 22:26:38 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,230 @@
+import datetime
+from utils import *
+import streamlit as st
+
+
+st.set_page_config(page_title="visualization project", layout='wide')
+header = st.container()
+fig1 = st.container()
+fig2 = st.container()
+fig3 = st.container()
+
+util = Utils()
+
+st.markdown("""
+    <style>
+    .desc {
+        font-size:22px;
+        color: #626862;
+        font-weight: bold;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.markdown("""
+    <style>
+    .warn {
+        font-size:22px;
+        color: #FF0000;
+        font-weight: bold;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+with header:
+    title = 'How Americans Spend Their Time'
+
+    project_description = """
+    In this project we provide visualization tools that might help to analyze Americans' time investment<br> Goals are
+    to understand what activities are most common and how different demographic/economic factors might affect them.
+    <br><br>
+    Our data is based on "Americans Time Usage Survey" (ATUS) organization which includes around 3 million records
+     of different people
+    around the united states and how they invest their time on a daily basis.<br><br>
+    """
+
+    st.markdown(f"<h1 style='text-align: center; color: black; font-size: 60px;'>{title}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h3 style='text-align: center; color: #626862; font-size: 30px;'>{project_description}</h3>",
+                unsafe_allow_html=True)
+
+with fig1:
+    st.markdown(f"<h3 style='color: black; font-size: 40px;'>Popular Activities - Children a Factor?</h3>",
+                unsafe_allow_html=True)
+
+    st.markdown(f'<p class="desc">Bar plot showing how many Americans do each of the activities in the chosen'
+                ' time range.<br>'
+                'Each activity counts number of Americans of two groups:<br>'
+                '1. With small kids (<18)<br>'
+                '2. Without kids or kids that are >18<br>'
+                'This might help to better understand if'
+                ' children are a factor that affects time usage among Americans<br><br>'
+                'Please choose time range below:</p>', unsafe_allow_html=True)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        start = st.time_input('Start Time', datetime.time(8, 00))
+    with col2:
+        stop = st.time_input('Stop Time', datetime.time(9, 15))
+
+    start = str(start)
+    stop = str(stop)
+
+    figure1 = util.fig1(start, stop)
+    figure1.update_layout(
+        xaxis=dict(
+            title=dict(
+                text='Activity',  # Set the desired x-axis title
+                font=dict(size=30)  # Set the desired font size for the x-axis title
+            ),
+            tickfont=dict(
+                size=20  # Set the desired font size for the x-axis ticks
+            )
+        ),
+        yaxis=dict(
+            title=dict(
+                text='Number (Log Scaled)',  # Set the desired y-axis title
+                font=dict(size=30)  # Set the desired font size for the y-axis title
+            ),
+            tickfont=dict(
+                size=20  # Set the desired font size for the y-axis ticks
+            )
+        ),
+        legend=dict(
+            font=dict(
+                size=22  # Set the desired font size for the legend labels
+            ),
+            title_font_size=24
+
+        )
+
+    )
+    st.plotly_chart(figure1, use_container_width=True)
+
+with fig2:
+    st.markdown(f"<h3 style='color: black; font-size: 40px;'>Activity Popularity By Geographical Location</h3>", unsafe_allow_html=True)
+    st.markdown('<p class="desc">The map shows the amount of Americans that do the specified activity in'
+                ' different geographical locations (states) based on the chosen time range.<br>'
+                'Choosing time and activity will make the map focus on relevant data only<br><br>'
+                'Numbers represent the proportion of people of the specified state of total people participated in'
+                ' this survey in this state.</p>', unsafe_allow_html=True)
+    col1, col2 = st.columns([3, 1])
+
+    with col2:
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.markdown('<p class="desc">Please choose desired time range and activity below:</p>', unsafe_allow_html=True)
+        col21, col22 = st.columns(2)
+
+        with col21:
+            start2 = st.time_input('Start time', datetime.time(8, 45), key='fig2_start')
+        with col22:
+            stop2 = st.time_input('End time', datetime.time(9, 45), key='fig2_stop')
+        start2 = str(start2)
+        stop2 = str(stop2)
+        activity = st.selectbox(
+            'Select activity',
+            ("Sports", "PersonalCare", "Socializing", "Eating", "CareGiving", "Travel", "Shopping",
+             "Housework", "Calls", "Work"))
+    with col1:
+        rows_count, count_per_state, figure2 = util.fig2(start2, stop2, activity)
+        figure2.update_layout(
+            coloraxis_colorbar=dict(
+                title='Proportion',
+                thicknessmode="pixels",
+                thickness=20,
+                lenmode="pixels",
+                len=300,
+                ticks="outside",
+                title_font_size=24,
+                tickfont=dict(size=20)  # Set the desired font size for the color legend
+            ),
+            annotations=[
+                dict(
+                    text=f'Total number of Americans in specified time frame: {rows_count}',
+                    xref='paper',
+                    yref='paper',
+                    x=0.5,
+                    y=1,
+                    showarrow=False,
+                    font=dict(size=22)
+                )
+            ]
+
+
+
+        )
+        st.plotly_chart(figure2, use_container_width=True)
+
+with fig3:
+    st.markdown(f"<h3 style='color: black; font-size: 40px;'>Activity Popularity Flow</h3>",
+                unsafe_allow_html=True)
+    st.markdown("<p class=desc>"
+                "A flow showing how sex and age affect the popularity of different activities. The flow consists of 3"
+                " layers that represent the splits in data:<br>"
+                "Left layer representing split based on sex - blue for male and pink for female."
+                " Middle layer splits data based on age.<br>"
+                "The flow can be used to understand which activities are more popular than others, "
+                "and define the its population.<br>"
+                "Based on chosen time and activities the flow will focus on relevant data<br><br>"
+                "Please choose time and up to 3 activities on the filter panel on the right."
+                "</p>", unsafe_allow_html=True)
+    st.markdown("<p class=warn>"
+                "Note: After 3 activities are chosen, any additional choices will be ignored!!"
+                "</p>", unsafe_allow_html=True)
+    col31, col32 = st.columns([9, 1])
+    with col32:
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        st.text(" ")
+        time = st.time_input('Choose time', datetime.time(8, 45), key='fig3_time')
+        time = str(time)
+
+        activities = ("Sports", "PersonalCare", "Socializing", "Eating", "CareGiving", "Travel",
+                      "Shopping", "Housework", "Calls", "Work")
+        cb_sports = st.checkbox("Sports")
+        cb_PersonalCare = st.checkbox("PersonalCare")
+        cb_Socializing = st.checkbox("Socializing")
+        cb_Eating = st.checkbox("Eating")
+        cb_CareGiving = st.checkbox("CareGiving")
+        cb_Travel = st.checkbox("Travel")
+        cb_Shopping = st.checkbox("Shopping")
+        cb_Housework = st.checkbox("Housework")
+        cb_Calls = st.checkbox("Calls")
+        cb_Work = st.checkbox("Work")
+
+        cb_acts = []
+        for activity, value in zip(activities,
+                                   [cb_sports, cb_PersonalCare, cb_Socializing, cb_Eating, cb_CareGiving, cb_Travel,
+                                    cb_Shopping, cb_Housework, cb_Calls, cb_Work]):
+            if value and len(cb_acts) < 3:
+                cb_acts.append(activity)
+
+    with col31:
+        total_records, figure3 = util.fig3(time, cb_acts)
+        figure3.update_layout(
+            annotations=[
+                dict(
+                    text=f'Total number of Americans in specified time frame: {total_records}',
+                    xref='paper',
+                    yref='paper',
+                    x=0.5,
+                    y=1.1,
+                    showarrow=False,
+                    font=dict(size=22)
+                )
+            ]
+        )
+        st.plotly_chart(figure3, use_container_width=True)
---
+++ utils.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+
+
+class Utils:
+
+    def __init__(self):
+        self.merged_df = pd.read_csv('merged_df.csv')
+
+    def fig1(self, start, stop):
+        activities = ["Sports", "PersonalCare", "Socializing", "Eating", "CareGiving", "Travel", "Shopping",
+                      "Housework", "Calls", "Work"]
+        data = self.merged_df[
+            ['tucaseid', 'tuactivity_n', 'tustarttim', 'tustoptime', 'activity_name', 'has_children_under_18']]
+
+        filtered_df = data[(data['tustarttim'] >= start) & (data['tustoptime'] <= stop)]
+
+        # Change dtype of a column
+        children_mapping = {0: 'No',
+                            1: 'Yes'}
+        filtered_df['has_children_under_18'] = filtered_df['has_children_under_18'].map(children_mapping)
+        fig = px.histogram(filtered_df, x='activity_name', color='has_children_under_18', barmode='group',
+                           color_discrete_sequence={0: '#FF9B01', 1: '#6177F1'},
+                           category_orders={'activity_name': activities},
+                           labels={'has_children_under_18': 'Have children under 18'},
+                           log_y=True
+                           ).update_xaxes(categoryorder="total descending")
+        fig.update_traces(texttemplate='%{y}', textposition='outside', textfont=dict(size=14))
+
+        # Remove y-axis labels
+        fig.update_layout(yaxis=dict(title='', showticklabels=False))
+        fig.update_layout(legend=dict(title='Has Children Under 18',
+                                      yanchor='top',
+                                      y=0.99,
+                                      xanchor='left',
+                                      x=0.99,
+                                      font_size=20,
+                                      title_font_size=24
+                                      ))
+        return fig
+
+    def fig2(self, start, stop, activity):
+
+        data2 = self.merged_df[['tucaseid', 'activity_name', 'tustarttim', 'tustoptime', 'gestfips']]
+
+        filtered_df = data2[(data2['tustarttim'] >= start) & (data2['tustoptime'] <= stop) & (
+            data2['activity_name'] == activity)]
+
+        state_counts = filtered_df.groupby('gestfips').size().reset_index(name='count')
+        total_counts = self.merged_df.groupby(['gestfips']).size().reset_index(name='total_in_state')
+
+        new_df = state_counts.merge(total_counts, left_on='gestfips', right_on='gestfips', how='left')
+        new_df['normalized_count'] = round(new_df['count'] / new_df['total_in_state'], 4)
+        rows_count = len(filtered_df)
+
+        fig = px.choropleth(data_frame=new_df, locationmode='USA-states', locations='gestfips',
+                            labels={'normalized_count': 'Normalized Count'},
+                            scope='usa', color='normalized_count',
+                            color_continuous_scale='Blues',
+                            hover_data=['count', 'normalized_count', 'gestfips'])
+        fig.update_layout(geo=dict(bgcolor='rgba(0,0,0,0)', lakecolor='rgba(0,0,0,0)'), height=700, width=1300)
+        fig.update_traces(
+            hovertemplate='<b>State FIPS code: %{customdata[2]}</b><br>'
+                          '<b>Number of people: %{customdata[0]}</b><br>'
+                          '<b>Proportion of state population: %{customdata[1]}</b><br>'
+                          ,
+            hoverlabel=dict(
+                font=dict(size=20)  # Set the desired font size for the hover text
+            )
+        )
+        return rows_count, state_counts, fig
+
+    def fig3(self, time, activity):
+
+        data3 = self.merged_df[['tucaseid', 'activity_name', 'tustarttim', 'tustoptime','teage','tesex']]
+
+        # Filter dataframe based on start and stop time, and activity
+        df = data3[(data3['tustarttim'] <= time) & (data3['tustoptime'] >= time) & data3['activity_name'].isin(
+            activity)]
+
+        # Create age groups
+        bins = [0, 18, 30, 45, 60, 75, np.inf]
+        names = ['<18', '18-30', '30-45', '45-60', '60-75', '75+']
+
+        df['age_group'] = pd.cut(df['teage'], bins, labels=names)
+
+        # Group data by sex, age group and activity and count the records
+        group = df.groupby(['tesex', 'age_group', 'activity_name']).size().reset_index(name='count')
+
+        # Get unique labels and add them to the nodes list
+        sex_labels = sorted(group['tesex'].apply(lambda x: 'Male' if x == 1 else 'Female').unique().tolist(), reverse=True)
+        age_labels = sorted(group['age_group'].unique().tolist())
+        activity_labels = group['activity_name'].unique().tolist()
+
+        labels = sex_labels + age_labels + activity_labels
+
+        male_color = '#66B2FF'
+        female_color = '#FFA2A2'
+
+        # Create source, target, value and color lists
+        source = []
+        target = []
+        value = []
+        color = []
+
+        # Loop over each record and add to source, target, value and color lists
+        total_records = 0
+        max_count = group['count'].max()
+        for i, row in group.iterrows():
+            if row['count'] < 0.1*max_count:
+                continue
+            source.append(labels.index('Male' if row['tesex'] == 1 else 'Female'))
+            target.append(labels.index(row['age_group']))
+            value.append(row['count'])
+            color.append(male_color if row['tesex'] == 1 else female_color)
+
+            source.append(labels.index(row['age_group']))
+            target.append(labels.index(row['activity_name']))
+            value.append(row['count'])
+            color.append(male_color if row['tesex'] == 1 else female_color)
+            total_records += row['count']
+
+        # Create Sankey diagram
+        fig = go.Figure(data=[go.Sankey(
+            node=dict(
+                pad=50,
+                thickness=5,
+                line=dict(color="black", width=0.1),
+                label=labels,
+                color="black",
+            ),
+            link=dict(
+                source=source,
+                target=target,
+                value=value,
+                color=color,
+                line=dict(color='black', width=0.5),
+
+
+            )
+        )])
+
+        fig.update_layout(height=1000, font_size=40)
+        fig.update_traces(textfont_color='black', selector=dict(type='sankey'))
+        fig.update_traces(
+            # hovertemplate='<b>Number of people: %{customdata[0]}</b><br>Proportion of state population: %{customdata[1]}',
+            hoverlabel=dict(
+                font=dict(size=30)  # Set the desired font size for the hover text
+            )
+        )
+
+        return total_records, fig
